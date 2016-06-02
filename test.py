6604f15ec355e1bba60f94ef501c9eecbc1e32fb
zijuexiansheng <zijuexiansheng@gmail.com>
2016-06-02 12:17:42 -0400

    test modified

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-a = (i*i for i in range(10))
-for each in a:
-	print each
+#!/usr/bin/env python
+
+print "import test.py"
+
+if __name__ == '__main__':
+    a = (i * i for i in range(10))
+    for each in a:
+        print each
 
